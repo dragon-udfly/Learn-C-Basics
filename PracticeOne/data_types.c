b8053fca224d53da5e8d033641ed2c06b8ae2070
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-09 20:46:34 +0530

    Created variables

---
+++ PracticeOne/data_types.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+    int width = 232;
+    int height = 322;
+    int particles = 93233;
+
+    float rate = 0.3;
+    float value_error = 0.1;
+    float diameter = 3.22;
+
+    double planet_diameter = 23003.3;
+    double growth_rate = 0.003;
+    double weight = 32.3;
+
+    char grade = 'B';
+    char symbol = '^';
+    char placeholder = '.';
+
+    char name[] = "Dragon Ud Fly";
+    char place[] = "New York";
+    char random_text[] = "tex3023&(323)3233";
+
+    return 0;
+}
