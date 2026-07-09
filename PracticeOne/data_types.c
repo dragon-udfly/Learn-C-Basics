2ca91d34e55c05b35bbb862e1ea587507e96fb1d
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-09 20:51:58 +0530

    Printed variables values

--- PracticeOne/data_types.c
+++ PracticeOne/data_types.c
@@ -22,5 +22,21 @@ int main() {
     char place[] = "New York";
     char random_text[] = "tex3023&(323)3233";
 
+    printf("Width: %d\n", width);
+    printf("Height: %d\n", height);
+    printf("Particles: %d\n", particles);
+    printf("Rate: %.1f\n", rate);
+    printf("Error of Value: %.1f\n", value_error);
+    printf("Diameter: %.2f\n", diameter);
+    printf("Diameter of Planet: %.1lf\n", planet_diameter);
+    printf("Growth Rate: %.3lf\n", growth_rate);
+    printf("Weight: %.1lf\n", weight);
+    printf("Grade: %c\n", grade);
+    printf("Symbol: %c\n", symbol);
+    printf("Placeholder: %c\n", placeholder);
+    printf("Name: %s\n", name);
+    printf("Place: %s\n", place);
+    printf("Random Text: %s\n", random_text);
+
     return 0;
 }
