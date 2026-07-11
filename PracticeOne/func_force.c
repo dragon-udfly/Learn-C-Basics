4a056cd68dff0cb39a8984cd2f112dd29cf083c9
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-11 15:20:58 +0530

    add: implement force calculation program

---
+++ PracticeOne/func_force.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+float calculateForce(float mass, float acceleration) {
+    return (mass * acceleration);
+}
+
+int main() {
+
+    float values_pairs[][2] = {
+        {34.2, 0.2}, 
+        {56.2, 0.3}, 
+        {45.9, 0.2}, 
+        {13.2, 1.0}, 
+        {10.2, 2.3}, 
+        {30.9, 1.2}
+    };
+
+    for(int i = 0; i < (sizeof(values_pairs) / sizeof(values_pairs[0])); i++) {
+        printf("Force: %.1f\n", calculateForce(values_pairs[i][0], values_pairs[i][1]));
+    }
+
+    return 0;
+}
