cc5b8622c2aba25920ff3cdb3d40d735f771732a
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-11 15:27:47 +0530

    add: implement resistance calculation in force program

--- PracticeOne/func_force.c
+++ PracticeOne/func_force.c
@@ -4,6 +4,10 @@ float calculateForce(float mass, float acceleration) {
     return (mass * acceleration);
 }
 
+float calculateResistance(float voltage, float current) {
+    return (voltage / current);
+}
+
 int main() {
 
     float values_pairs[][2] = {
@@ -17,6 +21,10 @@ int main() {
 
     for(int i = 0; i < (sizeof(values_pairs) / sizeof(values_pairs[0])); i++) {
         printf("Force: %.1f\n", calculateForce(values_pairs[i][0], values_pairs[i][1]));
+    }
+
+    for(int i = 0; i < (sizeof(values_pairs) / sizeof(values_pairs[0])); i++) {
+        printf("Resistance: %.1f\n", calculateResistance(values_pairs[i][0], values_pairs[i][1]));
     }
 
     return 0;
