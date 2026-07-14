b7cc1b1f61bf02afd5b6d1a138b32575aad6bcf0
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-14 11:57:16 +0530

    add: create define_o.c with MAX_USERS and kMaxInputs constants

---
+++ PracticeOne/define_o.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define MAX_USERS 5
+
+const int kMaxInputs = 5;
+
+int main() {
+    printf("Max Users: %d\n", MAX_USERS);
+    printf("Maximum Inputs: %d\n", kMaxInputs);
+
+    return 0;
+}
