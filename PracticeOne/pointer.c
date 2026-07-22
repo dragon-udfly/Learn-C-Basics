93315aa93fd177ce615567596e06e02aec4dec56
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-22 10:46:57 +0530

    Using NULL pointer

--- PracticeOne/pointer.c
+++ PracticeOne/pointer.c
@@ -13,5 +13,12 @@ int main() {
     // to get the real value of pointed variable
     printf("Value: %d\n", *y);
 
+    // null pointer 
+
+    int age = 23;
+    int *address_age = NULL; // good to assign NULL when declaring
+    address_age = &age;
+    printf("Value: %d, Address: %x", *address_age, address_age);
+
     return 0;
 }
