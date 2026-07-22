f434f00b657e01945747f4c5391092cfbcff2a22
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-22 10:23:33 +0530

    Created file to get memory address

---
+++ PracticeOne/pointer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    int value = 10; // variable
+
+    // printing variables and its memory address
+    printf("Value: %d, Address: %x", value, &value);
+
+    return 0;
+}
