69c590e8472299fe1ab8ddf02a41be2a8fc38b69
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-22 10:35:28 +0530

    assigning memory address of variable

--- PracticeOne/pointer.c
+++ PracticeOne/pointer.c
@@ -7,7 +7,9 @@ int main() {
     // printing variables and its memory address
     printf("Value: %d, Address: %x\n", value, &value);
 
-    int *y;
+    int *y; // declare pointer variable to store memory address
+    y = &value; // assigning memory address of variable: value
+    printf("Address: %x\n", y);
 
     return 0;
 }
