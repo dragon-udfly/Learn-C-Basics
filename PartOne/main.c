9753692c09e523daca004be2c3cb4e59b1b425a7
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-22 11:53:38 +0530

    Updated main.c file

--- PartOne/main.c
+++ PartOne/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv) {
 
-    printf("Starting Of The Program.\n");
+    FILE* file_object = fopen("file.txt", "r");
+    if(file_object == NULL) {
+        perror("Unable to open file");
+        return EXIT_FAILURE;
+    }
+
+    char buffer[512];
+    while(fgets(buffer, sizeof(buffer), file_object) != NULL) {
+        printf("%s", buffer);
+    }
+    printf("\n");
+
+    // check the first line
+    int white_spaces = 0;
+    for(int i = 0; i < sizeof(buffer); i++) {
+        if(buffer[i] == ' ') {
+            white_spaces++;
+        }
+    }
+    printf("Number of White Spaces: %d\n", white_spaces);
+
+
     return 0;
 }
