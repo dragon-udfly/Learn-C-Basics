14670aeceb1948fd7fbe2f58466dec1361e20638
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-07-22 10:23:47 +0530

    Added /n

--- PracticeOne/pointer.c
+++ PracticeOne/pointer.c
@@ -5,7 +5,7 @@ int main() {
     int value = 10; // variable
 
     // printing variables and its memory address
-    printf("Value: %d, Address: %x", value, &value);
+    printf("Value: %d, Address: %x\n", value, &value);
 
     return 0;
 }
